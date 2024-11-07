4be2d4abef25b8bebe837544ca4ecf2007c1200e
LagGap <149432711+LagGap@users.noreply.github.com>
2024-11-07 13:04:45 -0500

    testing with vue

--- MyWebsite/src/App.vue
+++ MyWebsite/src/App.vue
@@ -1,47 +1,27 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      message: "allo",
+      isVisible: true
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    },
+    changeMessageVisibility() {
+      this.isVisible = !this.isVisible
+    }
+  },
+}
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1 v-if="isVisible">{{ message }}</h1>
+    <button @click="increment">Count is : {{ count }}</button>
+    <button @click="changeMessageVisibility">Change Message Visibility</button>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
