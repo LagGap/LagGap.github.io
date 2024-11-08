7a53e18026cf5883b03e7e9a7e04b382d4aaf96d
LagGap <149432711+LagGap@users.noreply.github.com>
2024-11-08 14:07:22 -0500

    footer et contact ajouter sans le css

--- my-website/src/app/app.component.ts
+++ my-website/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ContactComponent } from "./contact/contact.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [NavbarComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-website';
+  //https://krahab.itch.io/warbrigade
 }
