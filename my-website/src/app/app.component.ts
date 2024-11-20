95628df180afba3206c3dc361201419658eca08f
LagGap <149432711+LagGap@users.noreply.github.com>
2024-11-20 15:32:45 -0500

    bouton CV fonctionnel

--- my-website/src/app/app.component.ts
+++ my-website/src/app/app.component.ts
@@ -3,11 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ContactComponent } from "./contact/contact.component";
 import { FooterComponent } from "./footer/footer.component";
+import { CvComponent } from "./cv/cv.component";
+import { MyProjectsComponent } from "./my-projects/my-projects.component";
+import { AboutComponent } from "./about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, ContactComponent, FooterComponent],
+  imports: [NavbarComponent, ContactComponent, FooterComponent, CvComponent, MyProjectsComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- my-website/src/app/cv/cv.component.ts
+++ my-website/src/app/cv/cv.component.ts
@@ -5,8 +5,17 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './cv.component.html',
-  styleUrl: './cv.component.css'
+  styleUrl: './cv.component.css',
 })
 export class CvComponent {
-
+  downloadElement() {
+    console.log('allo');
+    let link = document.createElement('a');
+    link.setAttribute('type', 'hidden');
+    link.href = 'resources/CV_GL.pdf';
+    link.download = '';
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  }
 }
